8701d37295c512cbd6817a8d9836b08b3b48dc71
kwa <kwaisloading@Kickins-MacBook-Pro.local>
2022-03-12 09:25:17 +0900

    Clone Hydrogen demo

---
+++ shopify.config.js
@@ -0,0 +1,12 @@
+export default {
+  storeDomain: 'hydrogen-preview.myshopify.com',
+  storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
+  storefrontApiVersion: '2022-01',
+};
+
+
+// export default {
+//   storeDomain: 'statlas-demo.myshopify.com',
+//   storefrontToken: '65e53fc3cc81e48c23e43d2ee4771685',
+//   storefrontApiVersion: '2022-01',
+// };
